316a7e4e723a08e8902566e7d5bcf73450854f15
Emilio López <turl@tuxfamily.org>
2012-05-25 01:55:08 +0000

    Include android.hardware.location.xml
    We can support wifi network location, so we need to include this

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ DEVICE_PACKAGE_OVERLAYS := device/allwinner/common/overlay
 PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
 	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
 	frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
