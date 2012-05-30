908b5b9246e5ae6c19efe063bf2627cc41940018
Emilio López <turl@tuxfamily.org>
2012-05-30 19:51:43 +0000

    Allow per-device extension of postrecoveryboot.sh, and adjust governor

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -1,3 +1,9 @@
 #!/sbin/sh
 /sbin/busybox dd if=/dev/zero count=1 | /sbin/busybox tr '\000' '\377' > /dev/block/nandf
 /sbin/sync
+
+/sbin/echo fantasy > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+if [ -e /sbin/devicespecific.sh ]; then
+    . /sbin/devicespecific.sh
+fi
