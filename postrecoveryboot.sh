29cafc8b21530aae2d33278a9454826ba70f471a
Emilio López <turl@tuxfamily.org>
2012-06-29 00:37:05 +0000

    Clean misc partition on boot

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -1,5 +1,6 @@
 #!/sbin/sh
 /sbin/echo fantasy > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+/sbin/cat /dev/zero > /dev/block/nandf
 
 if [ -e /sbin/devicespecific.sh ]; then
     . /sbin/devicespecific.sh
