afc821a43b017b7cddfafc08dd47e9bb38ab95e7
Jari Helaakoski <tekkuli@gmail.com>
2013-03-23 20:14:12 +0200

    Add files required by init.rc and ueventd.rc
    init.rc seems to require import /init.${ro.hardware}.rc

--- device.mk
+++ device.mk
@@ -87,8 +87,10 @@ PRODUCT_PACKAGES += libCedarA \
 PRODUCT_COPY_FILES += device/allwinner/common/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
 # and let's get booting!
-PRODUCT_COPY_FILES += device/allwinner/common/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
-	device/allwinner/common/init.sun4i.rc:root/init.sun4i.rc \
+PRODUCT_COPY_FILES += device/allwinner/common/ueventd.sunxi.rc:root/ueventd.sun4i.rc \
+	device/allwinner/common/ueventd.sunxi.rc:root/ueventd.sun5i.rc \
+	device/allwinner/common/init.sunxi.rc:root/init.sun4i.rc \
+	device/allwinner/common/init.sunxi.rc:root/init.sun5i.rc \
 	device/allwinner/common/init.sunxi.usb.rc:root/init.sunxi.usb.rc \
 #
 
